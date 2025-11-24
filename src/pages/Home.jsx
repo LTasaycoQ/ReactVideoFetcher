27d606f00dba0b634325fdb38bddaca747d956ab
Luis Tasayco <luistasayco3030@gmail.com>
2025-11-24 08:22:05 +0000

    mostrar audio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -372,6 +372,78 @@ function Home() {
                 </div>
               </div>
 
+              <div className='containerAudio bg-[#2A2D3C] hidden max-md:flex max-sm:flex-col items-center  max-sm:text-center  w-full  rounded-xl '>
+                <div className="seccionAudio bg-cover no-repeat bg-[url('https://img.freepik.com/foto-gratis/ilustracion-cielo-nocturno-anime_23-2151684328.jpg?semt=ais_hybrid&w=740&q=80')] flex items-center  max-sm:text-center  w-full  rounded-xl">
+                  <div className='w-[30%] max-sm:w-[40%] items-center flex justify-center'>
+                    <img src="https://canalmuseal.com/wp-content/uploads/2025/09/disco-naranja-vinilo-.png" className="w-[120px] h-[120px] max-sm:w-[90px] max-sm:h-[90px] rounded-full " />
+                  </div>
+
+                  <div className='w-[70%] max-sm:w-[60%] flex flex-col gap-[10px]'>
+                    <div className='contenedorTittleAudio flex justify-between w-[80%]  items-center rounded-full '>
+                      <div className='flex items-center gap-[10px]'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-apple-music" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="m10.995 0 .573.001q.241 0 .483.007c.35.01.705.03 1.051.093.352.063.68.166.999.329a3.36 3.36 0 0 1 1.47 1.468c.162.32.265.648.328 1 .063.347.084.7.093 1.051q.007.241.007.483l.001.573v5.99l-.001.573q0 .241-.008.483c-.01.35-.03.704-.092 1.05a3.5 3.5 0 0 1-.33 1 3.36 3.36 0 0 1-1.468 1.468 3.5 3.5 0 0 1-1 .33 7 7 0 0 1-1.05.092q-.241.007-.483.008l-.573.001h-5.99l-.573-.001q-.241 0-.483-.008a7 7 0 0 1-1.052-.092 3.6 3.6 0 0 1-.998-.33 3.36 3.36 0 0 1-1.47-1.468 3.6 3.6 0 0 1-.328-1 7 7 0 0 1-.093-1.05Q.002 11.81 0 11.568V5.005l.001-.573q0-.241.007-.483c.01-.35.03-.704.093-1.05a3.6 3.6 0 0 1 .329-1A3.36 3.36 0 0 1 1.9.431 3.5 3.5 0 0 1 2.896.1 7 7 0 0 1 3.95.008Q4.19.002 4.432 0h.573zm-.107 2.518-4.756.959H6.13a.66.66 0 0 0-.296.133.5.5 0 0 0-.16.31c-.004.027-.01.08-.01.16v5.952c0 .14-.012.275-.106.39-.095.115-.21.15-.347.177l-.31.063c-.393.08-.65.133-.881.223a1.4 1.4 0 0 0-.519.333 1.25 1.25 0 0 0-.332.995c.031.297.166.582.395.792.156.142.35.25.578.296.236.047.49.031.858-.043.196-.04.38-.102.555-.205a1.4 1.4 0 0 0 .438-.405 1.5 1.5 0 0 0 .233-.55c.042-.202.052-.386.052-.588V6.347c0-.276.08-.35.302-.404.024-.005 3.954-.797 4.138-.833.257-.049.378.025.378.294v3.524c0 .14-.001.28-.096.396-.094.115-.211.15-.348.178l-.31.062c-.393.08-.649.133-.88.223a1.4 1.4 0 0 0-.52.334 1.26 1.26 0 0 0-.34.994c.03.297.174.582.404.792a1.2 1.2 0 0 0 .577.294c.237.048.49.03.858-.044.197-.04.381-.098.556-.202a1.4 1.4 0 0 0 .438-.405q.173-.252.233-.549a2.7 2.7 0 0 0 .044-.589V2.865c0-.273-.143-.443-.4-.42-.04.003-.383.064-.424.073" />
+                        </svg>
+                        <p className='font-black text-[24px] max-sm:text-[18px] '>{data.nickname}</p>
+
+                      </div>
+                      <img src={data.avatar} alt="icono_creador" className='w-[30px] h-[30px] max-sm:hidden  rounded-full' />
+
+                    </div>
+                    <div className="containerDescargaAudio flex max-sm:hidden gap-[6px] items-center   w-full  containerReproductor">
+
+                      <div className='flex '>
+
+                        <button type='button' onClick={reproductorAudio} className='btnAudioReproductor w-[60px] bg-[#5252E5] h-[60px] rounded-full flex items-center justify-center'>
+                          {stateReproducto ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
+                            </svg>
+                          ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
+                            </svg>
+                          )}
+
+                        </button>
+
+
+                      </div>
+
+                      <a href={data.linkAudio} download className="text-center btnDescargarAudio w-full  rounded-xl bg-[#5252E5] font-black">Descargar Audio</a>
+
+                      <audio ref={audioRef} id='audioReproductor' className='hidden' src={data.linkAudio} controls ></audio>
+                    </div>
+
+
+                  </div>
+
+                </div>
+                <div className=' containerbtnDownload w-full max-sm:flex hidden'>
+                  <div className='flex items-center w-full gap-[15px]'>
+                    <div>
+                      <button type='button' onClick={reproductorAudio} className='btnAudioReproductor w-[60px] bg-[#5252E5] h-[60px] max-sm:w-[47px] max-sm:h-[47px] rounded-full flex items-center justify-center'>
+                        {stateReproducto ? (
+                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z" />
+                          </svg>
+                        ) : (
+                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pause-circle-fill" viewBox="0 0 16 16">
+                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
+                          </svg>
+                        )}
+
+                      </button>
+                    </div>
+                    <a href={data.linkAudio} download className="text-center btnDescargarAudio w-full  rounded-xl bg-[#5252E5] font-black">Descargar Audio</a>
+
+
+                  </div>
+                </div>
+
+              </div>
+
+
             </div>)
             : (
 
